4a7bf47092d214a7a5f6e768dfb53675d56bdecb
pedro-mendonca <ped.gaspar@gmail.com>
2023-10-26 16:57:00 +0100

    Fix glossary matching of terms left bounded by placeholders and numbered placeholders with more than 9

--- gp-templates/helper-functions.php
+++ gp-templates/helper-functions.php
@@ -213,14 +213,15 @@ function map_glossary_entries_to_translation_originals( $translation, $glossary 
 		}
 
 		// Build the regular expression.
-		$terms_search = '(%(?:[1-9]\$)?[bcdefgosuxEFGX%l@])|\b(';
+		$placeholders_search = '%(?:[1-9]\d*\$?(?:[1-9]\d*)?)?[bcdefgosuxEFGX%l@]';
+		$terms_search        = '(?:(\b|' . $placeholders_search . ')(';
 		foreach ( $regex_group as $suffix => $terms ) {
 			$terms_search .= '(?:' . implode( '|', $terms ) . ')' . $suffix . '|';
 		}
 
 		// Remove the trailing |.
 		$terms_search  = substr( $terms_search, 0, -1 );
-		$terms_search .= ')';
+		$terms_search .= ')\b)|(' . $placeholders_search . ')';
 	}
 	// Split the singular string on glossary terms boundaries.
 	$singular_split = preg_split( '/' . $terms_search . '/i', $translation->singular, 0, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE );
